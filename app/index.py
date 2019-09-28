8d3094bd3bc38e8f23374980fb93d1b0bc6cafb4
Luca Christen <lucachristen94@gmail.com>
2019-09-28 02:12:25 +0200

    Setup API endpoints with fake data

--- app/index.py
+++ app/index.py
@@ -3,18 +3,35 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/api/post_something', methods=['POST'])
-def api_post_something():
+@app.route('/api/votes', methods=['POST'])
+def api_post_votes():
     try:
         json_data = request.get_json()
-        return jsonify(**{"hello": json_data['hello']})
+        print(json_data)
+        return jsonify({}), 201
     except Exception as e:
         return jsonify(**{"error": e})
 
 
-@app.route('/api/get_something', methods=['GET'])
-def api_get_something():
-    return jsonify(**{"hello": "world"})
+@app.route('/api/votes', methods=['GET'])
+def api_get_votes():
+    try:
+        records = []
+        records.append({
+            "longitude": "0.0",
+            "latitude": "0.0",
+            "size" : "0",
+            "hotness" : "0"
+        })
+        records.append({
+            "longitude": "1.0",
+            "latitude": "1.0",
+            "size" : "1",
+            "hotness" : "1"
+        })
+        return jsonify(records)
+    except Exception as e:
+        return jsonify(**{"error": e})
 
 
 if __name__ == '__main__':
